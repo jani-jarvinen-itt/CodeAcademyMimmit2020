ce52f38bdf8eaa5fe3efbe8b2ba009482e6026e9
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-01-08 15:43:34 +0200

    Lisätty Entity Framework -esimerkki C#-kontrolleriin.

--- DotNet/AspNetWebApiDemo/Controllers/TestController.cs
+++ DotNet/AspNetWebApiDemo/Controllers/TestController.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using AspNetWebApiDemo.Models;
 
 namespace AspNetWebApiDemo.Controllers
 {
@@ -7,10 +10,29 @@ namespace AspNetWebApiDemo.Controllers
     [Route("[controller]")]
     public class TestController : ControllerBase
     {
+        /*
         [HttpGet]
         public string Kellonaika()
         {
             return DateTime.Now.ToString();
+        }
+        */
+
+        [HttpGet]
+        public List<Customer> Asiakkaat()
+        {
+            NorthwindContext context = new NorthwindContext();
+
+            return context.Customers.ToList();
+        }
+
+        [HttpGet]
+        [Route("tilaukset")]
+        public List<Order> Tilaukset()
+        {
+            NorthwindContext context = new NorthwindContext();
+
+            return context.Orders.ToList();
         }
     }
 }
