55b5bbe2b1247743f4b9cd7fdbc2e881a0bcdf62
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-02-11 10:36:06 +0200

    Aloitettu koodaamaan data-inetgraation esimerkkisovellusta.

---
+++ DataIntegraatioTesti/DataIntegraatioTesti/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataIntegraatioTesti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
