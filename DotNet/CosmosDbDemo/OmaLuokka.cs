126d06c220213fe1778bc8e00855e1e46a03e5a6
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-03-02 11:56:22 +0200

    Lisätty data-luokka sekä suurin osa Cosmos DB -käsittelykoodista.

---
+++ DotNet/CosmosDbDemo/OmaLuokka.cs
@@ -0,0 +1,13 @@
+namespace CosmosDbDemo {
+
+    public class OmaLuokka
+    {
+        public int Id { get; set; }
+
+        public int Luku { get; set; }
+
+        public string Nimi { get; set; }
+
+        public int[] Taulukko { get; set; }
+    }
+}
--- DotNet/CosmosDbDemo/Program.cs
+++ DotNet/CosmosDbDemo/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
 
 namespace CosmosDbDemo
 {
     class Program
     {
-        static void Main(string[] args)
+        public static string EndpointUrl { get; } = "https://codeacademymimmit.documents.azure.com/";
+        public static string PrimaryKey { get; } = "PPWplZzwOTXes7eMvIKu4rqYkIvkEyf3V62ZGRzMGlY0iYNX4YDqE4PG0z1vL0J1kYstfmnHB92ZweBacUndHw==";
+
+        // The Cosmos client instance
+        private static CosmosClient cosmosClient;
+
+        // The database we will create
+        private static Database database;
+
+        // The container we will create.
+        private static Container container;
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Aloitetaan Cosmos DB -tietokannan käsittely.");
+
+            database = await cosmosClient.CreateDatabaseIfNotExistsAsync("Mimmit2021");
+            Console.WriteLine("Tietokanta avattu: {0}", database.Id);
+
+            container = await database.CreateContainerIfNotExistsAsync("Testi", "/Id");
+            Console.WriteLine("Tallennuspaikka avattu: {0}", container.Id);
+
+            OmaLuokka data = new OmaLuokka()
+            {
+                Id = 10000,
+                Luku = 1234,
+                Nimi = "Maija Möttönen",
+                Taulukko = new int[] { 9, 8, 7, 6, 5 }
+            };
+
+            ItemResponse<OmaLuokka> vastaus = await container.CreateItemAsync<OmaLuokka>(
+                data, new PartitionKey(data.Id));
+
+            Console.WriteLine("Tallennettu data tietokantaan: {0}. Resurssikulutus: {1} RU:ta.",
+                vastaus.Resource.Id, vastaus.RequestCharge);
+
         }
     }
 }
